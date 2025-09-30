2c7a025e06b36c2c8b200e8894f87d87f17018b0
Anabelle Handdoek <github@huellaspyp.com>
2025-09-29 19:28:45 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
   },
   webpack5: true,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false };
+    config.resolve.fallback = { fs: false, path: false };
 
     return config;
   }
